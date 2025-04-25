a1cb90f6e6ba074296edb3ec754655fb08c9009a
David Souza <100738882+DavidSouzaLD@users.noreply.github.com>
2025-04-25 00:07:26 -0300

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -5,52 +5,68 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [Header("Basic")]
-    [SerializeField] private float moveSpeed = 15f;
-    [SerializeField] private float runSpeed = 20f;
-    [SerializeField] private float jumpHeight = 2f;
-    [SerializeField] private float gravityScale = 2f;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float gravityScale;
+
+    [Header("Collider")]
+    [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private float groundRayCheckDistance;
 
     [Header("Reference")]
     [SerializeField] private CharacterController m_CharacterController;
 
     private bool requestJump;
+    private bool isGrounded;
+    private bool isRunning;
     private float verticalVelocity;
 
     private void Awake()
     {
         m_CharacterController = GetComponent<CharacterController>();
-
         InputSystem.actions.FindAction("Jump").started += OnJump;
     }
 
     private void Update()
     {
+        isGrounded = GroundHittedObj().transform != null;
+        isRunning = InputSystem.actions.FindAction("Run").ReadValue<float>() != 0;
+
         Vector2 moveInput = InputSystem.actions.FindAction("Move").ReadValue<Vector2>().normalized;
         Vector3 forward = moveInput.x * transform.right + moveInput.y * transform.forward;
 
-        if (m_CharacterController.isGrounded && verticalVelocity < 0)
+        if (isGrounded && verticalVelocity < 0)
         {
             verticalVelocity = 0f;
         }
 
         // Jump
-        if (requestJump && m_CharacterController.isGrounded)
+        if (requestJump && isGrounded)
         {
             verticalVelocity = Mathf.Sqrt(jumpHeight * -2.0f * Physics.gravity.y * gravityScale);
             requestJump = false;
         }
 
         verticalVelocity += Time.deltaTime * Physics.gravity.y * gravityScale;
-        Vector3 finalMove = (Time.deltaTime * verticalVelocity * Vector3.up) + (moveSpeed * Time.deltaTime * forward);
+        Vector3 finalMove = (Time.deltaTime * verticalVelocity * Vector3.up) + ((isRunning ? runSpeed : moveSpeed) * Time.deltaTime * forward);
 
         m_CharacterController.Move(finalMove);
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (m_CharacterController.isGrounded)
+        if (isGrounded)
         {
             requestJump = true;
         }
+    }
+
+    private RaycastHit GroundHittedObj()
+    {
+        Vector3 initPoint = transform.position + m_CharacterController.center + (-transform.up * ((m_CharacterController.height / 2f) - 0.01f));
+        Vector3 endPoint = -transform.up * groundRayCheckDistance;
+        Debug.DrawRay(initPoint, endPoint, Color.magenta);
+        return Physics.Raycast(initPoint, endPoint, out RaycastHit hit, groundRayCheckDistance, groundLayers) ? hit : new();
     }
 }
